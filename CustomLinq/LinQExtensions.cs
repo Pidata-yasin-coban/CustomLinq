7c862b9d5e7b9967e78b563cc94c5f78548d9d6d
Yasin <yasin.coban@pidata.com.tr>
2023-02-20 02:17:09 +0300

    Proje dosyası ekle.

---
+++ CustomLinq/LinQExtensions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLinq
+{
+    public static class LinQExtensions
+    {
+        public static IEnumerable<UserModel> CustomWhere_EvenID(this IEnumerable<UserModel> users)
+        {
+            var result = new List<UserModel>();
+
+            foreach (var user in users)
+            {
+                if (user.Id % 2 == 0)
+                {
+                    result.Add(user);
+                }
+            }
+
+            return result;
+        }
+        public static IEnumerable<UserModel> CustomWhere_GmailUsers(this IEnumerable<UserModel> users)
+        {
+            var result = new List<UserModel>();
+
+            foreach (var user in users)
+            {
+                if (user.Email.EndsWith("gmail.com"))
+                {
+                    result.Add(user);
+                }
+            }
+
+            return result;
+        }
+        public static List<UserModel> CustomToList(this IEnumerable<UserModel> users)
+        {
+            var result = new List<UserModel>();
+
+            foreach (var user in users)
+            {
+                result.Add(user);
+            }
+
+            return result;
+        }
+        public static UserModel CustomFirstOrDefault(this IEnumerable<UserModel> users)
+        {
+            foreach (var user in users)
+            {
+                return user;
+            }
+
+            return null;
+        }
+
+        public static UserModel CustomFirstOrDefault_FirstNameLengthGrater4(this IEnumerable<UserModel> users)
+        {
+            foreach (var user in users)
+            {
+                if (user.FirstName.Length > 4)
+                {
+                    return user;
+                }
+               
+            }
+
+            return null;
+        }
+    }
+}
---
+++ CustomLinq/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+
+
+
+using CustomLinq;
+using System.Linq;
+
+var users = new List<UserModel>() {
+
+    new UserModel()
+    {
+        Id = 8,
+        FirstName= "Yasin1",
+        LastName= "Çoban",
+        CreatedDate= DateTime.Now,
+        Email =     "cobanyasin1998@gmail.com"
+    },
+     new UserModel()
+    {
+        Id = 2,
+        FirstName= "Yasin2",
+        LastName= "Çoban",
+        CreatedDate= DateTime.Now,
+        Email =     "cobanyasin1998@hotmail.com"
+    },
+      new UserModel()
+    {
+        Id = 1,
+        FirstName= "Yasin3",
+        LastName= "Çoban",
+        CreatedDate= DateTime.Now,
+        Email =     "cobanyasin1998@outlook.com"
+    },
+};
+var evenIDUsers = users.CustomWhere_EvenID().CustomToList();
+
+var firstUser = users.CustomFirstOrDefault();
+Console.WriteLine(firstUser?.FirstName);
+
+Console.WriteLine(evenIDUsers.Count());
+
+
+var linQUser = users
+    .Where(x => x.Id % 2 == 0)
+    .Where(x => x.Email.EndsWith("gmail.com"))
+    .ToList();
+
+var CustomLinQUser = users
+    .CustomWhere_EvenID()
+    .CustomWhere_GmailUsers()
+    .CustomToList();
+
+
+Console.ReadLine();
+//users.Where(x => x.FirstName == "Yasin").Select(x => x.FirstName);
